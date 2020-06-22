112554e59d6b4a32ef75249ca0eeac4187bcc8be
ada <851899570@qq.com>
2020-06-22 20:10:32 +0800

    add funnel funtion

--- funnel/src/visual.ts
+++ funnel/src/visual.ts
@@ -1,11 +1,22 @@
 import '../style/visual.less';
 import * as D3Funnel from 'd3-funnel';
+import _ = require('lodash')
 
+let isTooltipModelShown = false;
 export default class Visual extends WynVisual {
   private container: HTMLDivElement;
   private chart: any;
   private properties: any;
   private items: any;
+  private host: any;
+  private selectionManager: any;
+  private selection: any[] = [];
+  private _svg: SVGSVGElement;
+  private svg: any
+  private dimension: string
+  private ActualValue: string
+  private x: number
+  private y: number
 
   static mockItems = [
     { label: 'Inquiries', value: 5000 },
@@ -27,34 +38,63 @@ export default class Visual extends WynVisual {
         color: '#fff'
       },
     };
+    this.host = host;
+
 
     let dowebok: any = document.createElement("div");
     dowebok.setAttribute('id', 'funnel-container')
+
     this.container.appendChild(dowebok);
     this.chart = new D3Funnel('#funnel-container');
 
+    this.selectionManager = host.selectionService.createSelectionManager();
+  }
 
+
+  public getNodeSelectionId = (label: any, value: any) => {
+    const { selectionId } = this.items.find((item: any) => item.label === label && item.value === value)
+
+    if (!this.selectionManager.contains(selectionId)) {
+      this.selectionManager.select(selectionId, true);
+    } else {
+      this.selectionManager.clear(selectionId);
+    }
+    return selectionId
+  }
+
+  private clickHandler = (node: any) => {
+    this.host.toolTipService.show({
+      position: {
+        x: this.x,
+        y: this.y,
+      },
+      selectionId: this.getNodeSelectionId(node.label.raw, node.value),
+      selected: this.selectionManager.getSelectionIds(),
+      menu: true,
+    });
   }
 
   public render() {
+    this.host.eventService.renderStart();
     const options = this.properties;
-    const data = this.items.length ? this.items : Visual.mockItems;
+    const isMock = !this.items.length;
+    const data = isMock ? Visual.mockItems : this.items;
 
     const option = {
       chart: {
-        bottomWidth: 1 / options.bottomWidth, // 底部宽度
-        bottomPinch: options.bottomPinch, // 脖子长度, 数据的长度
-        inverted: options.inverted, //是否 反转
+        bottomWidth: 1 / options.bottomWidth,
+        bottomPinch: options.bottomPinch,
+        inverted: options.inverted,
         curve: {
-          enabled: options.curve, // 3D
-          height: options.curveHeight // 3D 的倾斜度
+          enabled: options.curve,
+          height: options.curveHeight
         },
         animate: options.animateEnabled ? options.animateTime : 0
       },
       block: {
         dynamicHeight: options.dynamicHeight,
         minHeight: 15,
-        barOverlay: options.barOverlay, //侧影
+        barOverlay: options.barOverlay,
         highlight: true,
         fill: {
           type: options.fillType
@@ -66,29 +106,53 @@ export default class Visual extends WynVisual {
         fontSize: options.textStyle.fontSize,
         fill: options.textStyle.color,
         format: '{l}: ${v}'
+      },
+      events: {
+        click: {
+          block: (e) => {
+            setTimeout(() => {
+              this.clickHandler(e)
+            })
+          }
+        }
       }
     };
 
+    document.addEventListener('click', (e) => {
+      this.x = e.clientX
+      this.y = e.clientY
+    })
+
     this.chart.draw(data, option);
 
+    this.host.eventService.renderFinish();
   }
 
   public update(options: VisualNS.IVisualUpdateOptions) {
-    console.log(options);
     const dataView = options.dataViews[0];
     this.items = [];
     if (dataView &&
       dataView.plain.profile.ActualValue.values.length && dataView.plain.profile.dimension.values.length) {
       const plainData = dataView.plain;
-      const dimension = plainData.profile.dimension.values[0].display;
-      const ActualValue = plainData.profile.ActualValue.values[0].display;
-      this.items = plainData.data.map((item: any) => {
-        let label = item[dimension]
-        let value = item[ActualValue]
-        return { label, value }
-      })
-    }
+      this.dimension = plainData.profile.dimension.values[0].display;
+      this.ActualValue = plainData.profile.ActualValue.values[0].display;
 
+      const items = plainData.data.reduce((result: any, item: any, i: number) => {
+        if (item[this.dimension] && item[this.ActualValue]) {
+          const selectionId = this.host.selectionService.createSelectionId();
+          selectionId
+            .withDimension(plainData.profile.dimension.values[0], item)
+            .withDimension(plainData.profile.ActualValue.values[0], item);
+          result.push({
+            label: item[this.dimension],
+            value: item[this.ActualValue],
+            selectionId,
+          });
+        }
+        return result;
+      }, []);
+      this.items = items;
+    }
     this.properties = options.properties;
     this.render()
   }
