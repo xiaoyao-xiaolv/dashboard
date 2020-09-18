1f964697085a9e41ea46edcaa469885cb4e647bb
xiaoyaoxiaolv <xiaoyaoxiaolv@gmail.com>
2020-09-18 09:12:56 +0800

    优化

--- 渐变仪表盘/src/visual.ts
+++ 渐变仪表盘/src/visual.ts
@@ -8,7 +8,7 @@ export default class Visual extends WynVisual {
   private properties: any;
   private ActualValue: any;
   private ContrastValue: any;
-  static mockItems = 0.5;
+  static mockItems = 50;
   constructor(dom: HTMLDivElement, host: VisualNS.VisualHost, options: VisualNS.IVisualUpdateOptions) {
     super(dom, host, options);
     this.container = dom;
@@ -31,33 +31,66 @@ export default class Visual extends WynVisual {
     this.properties = options.properties;
     this.render()
   }
+  public formatData = (number, dataUnit, dataType) => {
+    let format = number
+    // const dataUnit = options.totalValueUnit
+    const units = [{
+      value: 1,
+      unit: ''
+    },
+    {
+      value: 100,
+      unit: '百'
+    }, {
+      value: 1000,
+      unit: '千'
+    }, {
+      value: 10000,
+      unit: '万'
+    }, {
+      value: 100000,
+      unit: '十万'
+    }, {
+      value: 1000000,
+      unit: '百万'
+    }, {
+      value: 10000000,
+      unit: '千万'
+    }, {
+      value: 100000000,
+      unit: '亿'
+    }, {
+      value: 1000000000,
+      unit: '十亿'
+    }, {
+      value: 100000000000,
+      unit: '万亿'
+    }]
+    const formatUnit = units.find((item) => item.value === Number(dataUnit))
+    format = (format / formatUnit.value).toFixed(2)
+
+    if (dataType === 'number') {
+      format = format.toLocaleString()
+    } else if (dataType === '%') {
+      format = format + dataType
+    } else {
+      format = dataType + format
+    }
+    return format + formatUnit.unit
+  }
 
   private render() {
     this.chart.clear();
     const isMock = !this.items.length;
-    const items = isMock ? Visual.mockItems : this.items;
+    const items = isMock ? Visual.mockItems : (this.items[0] / this.items[1] * 100);
     this.container.style.opacity = isMock ? '0.3' : '1';
     const options = this.properties;
-    let color1 = [];
-    let color2 = [];
-    for (let i = 0; i < options.palette.length - 1; i++) {
-      color1.push([1 / options.palette.length * (i + 1), options.palette[i]])
-      color2.push({ offset: 1 / options.palette.length * (i + 1), color: options.palette[i] })
-    }
-    color1.push([1, options.palette[options.palette.length - 1]])
-    console.log(color2)
+    let num = isMock ? '' : this.formatData(this.items[0], options.detailValueUnit, options.detailValueType)
     let fontWeight: string;
     if (options.textStyle.fontWeight == "Light") {
       fontWeight = options.textStyle.fontWeight + "er"
     } else {
       fontWeight = options.textStyle.fontWeight
-    }
-
-    let detailfontWeight: string;
-    if (options.detailTextStyle.fontWeight == "Light") {
-      detailfontWeight = options.detailTextStyle.fontWeight + "er"
-    } else {
-      detailfontWeight = options.detailTextStyle.fontWeight
     }
     let endAngle = options.startAngle - options.endAngle;
     var option = {
@@ -66,7 +99,7 @@ export default class Visual extends WynVisual {
           type: 'gauge',
           center: ['50%', '50%'],
           radius: '100%',
-          splitNumber: 30,
+          splitNumber: 20,
           min: options.min,
           max: options.max,
           startAngle: options.startAngle,
@@ -75,7 +108,28 @@ export default class Visual extends WynVisual {
             show: true,
             lineStyle: {
               width: 0,
-              color: color1
+              color: [
+                [0.05, 'rgba(204, 255, 51, 0.5)'],
+                [0.10, 'rgba(153, 255, 51, 0.5)'],
+                [0.15, 'rgba(102, 255, 51, 0.5)'],
+                [0.20, 'rgba(51, 255, 51, 0.5)'],
+                [0.25, 'rgba(51, 255, 102, 0.5)'],
+                [0.30, 'rgba(51, 255, 153, 0.5)'],
+                [0.35, 'rgba(51, 255, 204, 0.5)'],
+                [0.40, 'rgba(51, 255, 255, 0.5)'],
+                [0.45, 'rgba(51, 204, 255, 0.5)'],
+                [0.50, 'rgba(51, 153, 255, 0.5)'],
+                [0.55, 'rgba(51, 102, 255, 0.5)'],
+                [0.60, 'rgba(51, 51, 255, 0.5)'],
+                [0.65, 'rgba(102, 51, 255, 0.5)'],
+                [0.70, 'rgba(153, 51, 255, 0.5)'],
+                [0.75, 'rgba(204, 51, 255, 0.5)'],
+                [0.80, 'rgba(255, 51, 255, 0.5)'],
+                [0.85, 'rgba(255, 51, 204, 0.5)'],
+                [0.90, 'rgba(255, 51, 153, 0.5)'],
+                [0.95, 'rgba(255, 51, 102, 0.5)'],
+                [1, 'rgba(153, 153, 153, 0.5)']
+              ]
             }
           },
           axisTick: {
@@ -103,13 +157,13 @@ export default class Visual extends WynVisual {
           name: "实际值",
           type: 'gauge',
           center: ['50%', '50%'],
-          radius: '80%',
+          radius: '75%',
           min: options.min,
           max: options.max,
           startAngle: options.startAngle,
           endAngle: endAngle,
           title: {
-            show: options.showSubTitle,
+            show: options.showNum,
             offsetCenter: [0, '80%'],
             color: options.textStyle.color,
             fontSize: options.textStyle.fontSize.substr(0, 2),
@@ -120,21 +174,86 @@ export default class Visual extends WynVisual {
           axisLine: {
             show: true,
             lineStyle: {
-              width: 16,
+              width: 14,
               color: [
                 [
-                  0.8, new echarts.graphic.LinearGradient(
-                    0, 0, 1, 0, [
-                    , { offset: 0.09090909090909091, color: "#5edfff" }
-                    , { offset: 0.18181818181818182, color: "#ffd348" }
-                    , { offset: 0.2727272727272727, color: "#47f4c6" }
-                    , { offset: 0.36363636363636365, color: "#66a3ff" }
-                    , { offset: 0.4545454545454546, color: "#f38044" }
-                    , { offset: 0.5454545454545454, color: "#8484ee" }
-                    , { offset: 0.6363636363636364, color: "#fe9200" }
-                    , { offset: 0.7272727272727273, color: "#e27300" }
-                    , { offset: 0.8181818181818182, color: "#c45100" }
-                    , { offset: 0.9090909090909092, color: "#f44e3b" }
+                  items / 100, new echarts.graphic.LinearGradient(
+                    0, 0, 1, 0, [{
+                      offset: 0.05,
+                      color: '#cf3'
+                    },
+                    {
+                      offset: 0.10,
+                      color: '#9f3'
+                    },
+                    {
+                      offset: 0.15,
+                      color: '#6f3'
+                    },
+                    {
+                      offset: 0.20,
+                      color: '#3f3'
+                    },
+                    {
+                      offset: 0.25,
+                      color: '#3f6'
+                    },
+                    {
+                      offset: 0.30,
+                      color: '#3f9'
+                    },
+                    {
+                      offset: 0.35,
+                      color: '#3fc'
+                    },
+                    {
+                      offset: 0.40,
+                      color: '#3ff'
+                    },
+                    {
+                      offset: 0.45,
+                      color: '#3cf'
+                    },
+                    {
+                      offset: 0.50,
+                      color: '#39f'
+                    },
+                    {
+                      offset: 0.55,
+                      color: '#36f'
+                    },
+                    {
+                      offset: 0.60,
+                      color: '#33f'
+                    },
+                    {
+                      offset: 0.65,
+                      color: '#63f'
+                    },
+                    {
+                      offset: 0.70,
+                      color: '#93f'
+                    },
+                    {
+                      offset: 0.75,
+                      color: '#c3f'
+                    },
+                    {
+                      offset: 0.80,
+                      color: '#f3f'
+                    },
+                    {
+                      offset: 0.85,
+                      color: '#f3c'
+                    },
+                    {
+                      offset: 0.90,
+                      color: '#f39'
+                    },
+                    {
+                      offset: 0.95,
+                      color: '#f36'
+                    }
                   ]
                   )
                 ],
@@ -159,11 +278,11 @@ export default class Visual extends WynVisual {
           detail: {
             show: true,
             offsetCenter: [0, 0],
-            formatter: '{value|' + 0.8 + '}{unit|.' + 0.8.toString().split('.')[1] + '%}',
+            formatter: '{value|' + items.toFixed(1) + '}{unit|%}',
             rich: {
               value: {
-                fontSize: 48,
-                lineHeight: 48,
+                fontSize: 40,
+                lineHeight: 40,
                 color: '#fff'
               },
               unit: {
@@ -180,8 +299,8 @@ export default class Visual extends WynVisual {
             }
           },
           data: [{
-            value: 0.8,
-            name: '人员到岗进度'
+            value: items,
+            name: num
           }]
         },
       ]
