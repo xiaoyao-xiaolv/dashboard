9823cad2be77e419ae3b7c09188798631e27ef3b
xiaoyaoxiaolv <xiaoyaoxiaolv@gmail.com>
2020-09-18 11:22:03 +0800

    增加预警

--- 图片加载/src/visual.ts
+++ 图片加载/src/visual.ts
@@ -4,22 +4,21 @@ export default class Visual extends WynVisual {
   private container: HTMLDivElement;
   private visualHost: any;
   private properties: any;
-  private imgUrl: any;
+  private items: any;
+  static mockItems = 10000;
   constructor(dom: HTMLDivElement, host: VisualNS.VisualHost, options: VisualNS.IVisualUpdateOptions) {
     super(dom, host, options);
     this.container = dom;
     this.visualHost = host;
+    this.items = [];
   }
 
   public update(options: VisualNS.IVisualUpdateOptions) {
     const dataView = options.dataViews[0];
-    this.imgUrl = '';
-    console.log(dataView)
-    if (dataView &&
-      dataView.plain.profile.imgUrl.values.length) {
+    if (dataView) {
       const plainData = dataView.plain;
-      let imgUrlName = plainData.profile.imgUrl.values[0].display;
-      this.imgUrl = plainData.data[0][imgUrlName]
+      let values = plainData.profile.values.values[0].display;
+      this.items = [plainData.data[0][values]]
     }
     this.properties = options.properties;
     this.render();
@@ -27,24 +26,33 @@ export default class Visual extends WynVisual {
 
   private render() {
     this.container.innerHTML = "";
-    const isMock = !this.imgUrl.length;
+    const isMock = !this.items.length;
     this.container.style.opacity = isMock ? '0.3' : '1';
+    let items = isMock ? Visual.mockItems : this.items;
     var maskImage = new Image();
     const options = this.properties;
-    var that = this;
     maskImage.className = "maskimage"
-    maskImage.onload = function () {
-      that.container.appendChild(maskImage);
+    maskImage.onload = () => {
+      this.container.appendChild(maskImage);
     };
-    maskImage.src = this.imgUrl || this.visualHost.assetsManager.getImage('mockImg');
-
+    let interval = JSON.parse(options.Interval)
+    if (items >= interval[1].gte) {
+      if (items >= interval[0].gte) {
+        maskImage.src = this.visualHost.assetsManager.getImage(interval[0].color + 'Img');
+      } else {
+        maskImage.src = this.visualHost.assetsManager.getImage(interval[1].color + 'Img');
+      }
+    } else {
+      maskImage.src = this.visualHost.assetsManager.getImage(interval[2].color + 'Img');
+    }
   }
+
   public onDestroy() {
 
   }
 
   public onResize() {
-
+    this.render();
   }
 
   public getInspectorHiddenState(options: VisualNS.IVisualUpdateOptions): string[] {
