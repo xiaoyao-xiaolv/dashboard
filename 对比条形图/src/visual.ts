be4dcdba5bb9033d7670393e3ee3fcbc159acc91
xiaoyao-xiaolv <xiaoyaoxiaolv@gmail.com>
2021-03-23 16:10:27 +0800

    广州智能优化跳转

--- 对比条形图/src/visual.ts
+++ 对比条形图/src/visual.ts
@@ -8,16 +8,15 @@ export default class Visual {
   private properties: any;
   private isMock: boolean;
   private items: any;
-  private valueFormat: any;
+  private actualFormat: any;
+  private contrastFormat: any;
   private host: any;
-  private selection: any;
-  private selectionManager: any;
-  private isTooltipModelShown: boolean;
   static mockItems = [["人事部", "财务部", "销售部", "市场部", "采购部", "产品部", "技术部", "客服部", "后勤部"]
     , [58, 46, 47, 49, 59, 17, 25, 83, 34]
     , [74, 64, 78, 65, 79, 21, 28, 91, 38]
     , [78.38, 71.88, 60.26, 75.38, 74.68, 80.95, 89.29, 91.21, 89.47]
     , ["复工人数", "总人数"]
+    , [1, 1, 1, 1, 1, 1, 1, 1, 1]
   ];
   constructor(dom: HTMLDivElement, host: any) {
     this.container = dom;
@@ -34,97 +33,45 @@ export default class Visual {
       barEndColor: '#2563f9',
     };
     this.bindEvents();
-    this.isTooltipModelShown = false;
-    this.selection = [];
-    this.selectionManager = host.selectionService.createSelectionManager();
   }
-
-  private showTooltip = _.debounce((params) => {
-    this.isTooltipModelShown = true;
-    const fields = [{ label: params.name, value: `${params.data}%` }]
-    this.host.toolTipService.show({
-      position: {
-        x: params.event.event.x,
-        y: params.event.event.y,
-      },
-      fields,
-      selected: this.selectionManager.getSelectionIds(),
-      menu: true,
-    }, 10);
-  });
-
-  private hideTooltip = () => {
-    this.host.toolTipService.hide();
-    this.isTooltipModelShown = false;
-  }
-
-  private dispatch = (type, payload) => this.chart.dispatchAction({ type, ...payload });
-
   public bindEvents = () => {
     this.container.addEventListener('click', (e: any) => {
       if (!e.seriesClick) {
-        this.hideTooltip();
-        this.selection.forEach(item => this.dispatch('downplay', item));
-        this.selection = [];
-        this.selectionManager.clear();
         return;
       }
     })
-
     this.chart.on('click', (params) => {
       if (params.componentType !== 'series') return;
       params.event.event.seriesClick = true;
-
       let dataIndex = params.dataIndex;
-      let sid = this.items[5][dataIndex];
-      let selectedInfo = {
-        seriesIndex: 1,
-        dataIndex: dataIndex,
-      };
-
-      if (this.selectionManager.contains(sid)) {
-        this.dispatch('downplay', selectedInfo);
-        this.selectionManager.clear(sid);
-        return;
-      }
-
-      this.showTooltip(params);
-      this.selectionManager.select(sid, true);
-      this.dispatch('highlight', selectedInfo);
-      this.selection.push(selectedInfo);
+      window.top.open(this.properties.jumpUrl + '?params=[' + this.items[6][dataIndex] + ']')
     })
   }
 
   public update(options: any) {
     const dataView = options.dataViews[0];
-    console.log(dataView)
     this.isMock = !dataView;
     this.items = [[], [], [], [], [], [], []];
-    if (dataView &&
-      dataView.plain.profile.ActualValue.values.length && dataView.plain.profile.ContrastValue.values.length && dataView.plain.profile.dimension.values.length) {
+    if (dataView) {
       const plainData = dataView.plain;
       let dimension = plainData.profile.dimension.values[0].display;
       let ActualValue = plainData.profile.ActualValue.values[0].display;
       let ContrastValue = plainData.profile.ContrastValue.values[0].display;
       let Compare = plainData.profile.Compare.values[0].display;
+      let jump = plainData.profile.jump.values[0].display;
       let data = plainData.data;
       this.items[0] = plainData.sort[dimension].order;
       this.items[4] = [ActualValue, ContrastValue];
-      this.valueFormat = plainData.profile.ActualValue.options.valueFormat;
+      this.actualFormat = plainData.profile.ActualValue.options.valueFormat;
+      this.contrastFormat = plainData.profile.ContrastValue.options.valueFormat;
       for (let index = 0; index < data.length; index++) {
         data.forEach((item) => {
           if (item[dimension] == this.items[0][index]) {
             this.items[1].push(item[ActualValue]);
             this.items[2].push(item[ContrastValue]);
             this.items[3].push(parseFloat((item[ActualValue] / item[ContrastValue] * 100).toFixed(2)));
-            this.items[6].push(item[Compare]);
-            const getSelectionId = (item) => {
-              const selectionId = this.host.selectionService.createSelectionId();
-              selectionId.withDimension(plainData.profile.dimension.values[0], item);
-              selectionId.withDimension(plainData.profile.jump.values[0], item);
-              return selectionId;
-            }
-            this.items[5].push(getSelectionId(item));
+            this.items[5].push(item[Compare]);
+            this.items[6].push(item[jump]);
           }
         })
       }
@@ -137,7 +84,6 @@ export default class Visual {
     this.chart.clear();
     const options = this.properties;
     const items = this.isMock ? Visual.mockItems : this.items;
-    console.log(items)
     this.container.style.opacity = this.isMock ? '0.3' : '1';
     let fontWeight: string;
     if (options.textStyle.fontWeight == "Light") {
@@ -158,7 +104,7 @@ export default class Visual {
           for (var i = 0; i < items[0].length; i++) {
             if (items[0][i] === params.name) {
               // return items[4][0] + ":" + items[1][i] + "<br/>" + items[4][1] + ":" + items[2][i];
-              return items[4][0] + ":" + this.host.formatService.format(this.valueFormat, items[1][i]).toLocaleString() + "<br/>" + items[4][1] + ":" + this.host.formatService.format(this.valueFormat, items[2][i]).toLocaleString();
+              return items[4][0] + ":" + this.host.formatService.format(this.actualFormat, items[1][i]).toLocaleString() + "<br/>" + items[4][1] + ":" + this.host.formatService.format(this.contrastFormat, items[2][i]).toLocaleString();
             }
           }
         }
@@ -212,7 +158,7 @@ export default class Visual {
           show: options.showBarLabel,
           position: 'inside',
           formatter: (params) => {
-            return this.host.formatService.format(this.valueFormat, params.value).toLocaleString() + "%";
+            return params.value.toFixed(1) + "%";
           },
           color: options.labelTextStyle.color,
           fontSize: options.labelTextStyle.fontSize.substr(0, 2),
@@ -222,7 +168,7 @@ export default class Visual {
         },
         itemStyle: {
           color: (params) => {
-            return params.value / 100 > items[6][params.dataIndex] ? options.piecesColor[0] : params.value / 100 == items[6][params.dataIndex] ? options.piecesColor[1] : options.piecesColor[2]
+            return params.value / 100 > items[5][params.dataIndex] ? options.piecesColor[0] : params.value / 100 == items[5][params.dataIndex] ? options.piecesColor[1] : options.piecesColor[2]
           },
           barBorderRadius: 14
         }
