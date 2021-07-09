0ce3a8297e638ee92e902198e44d5ca0c0cdbd47
Crystal <1365991837@qq.com>
2021-07-09 12:12:40 +0800

    finish carousel picture

--- carouselPicture/src/visual.ts
+++ carouselPicture/src/visual.ts
@@ -1,37 +1,77 @@
 import '../style/visual.less';
-import {Swiper, Autoplay, SwiperOptions} from 'swiper';
+import Swiper from 'swiper';
+import SwiperCore, { Autoplay, Navigation, Pagination, EffectCoverflow, EffectFade, EffectCube, EffectFlip} from 'swiper/core';
 import 'swiper/swiper.less';
 import 'swiper/swiper-bundle.css';
 
+const EFFECT = {
+  fadeEffect: {
+    crossFade: true
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+  flipEffect: {
+    slideShadows: false,
+  }
+}
+const STYLENAME = {
+  fontFamily: 'font-family',
+  fontSize: 'font-size',
+  fontStyle: "font-style",
+  fontWeight: "font-weight",
+}
 
 export default class Visual extends WynVisual {
+  private container: HTMLDivElement;
+  private isMock: boolean;
+  private properties: any;
+  private swiper: any;
+  private swiperOptions: any;
+  private nextBtn: any;
+  private prevBtn: any;
+  private prevEffect: any;
+  private firstRender: any;
+  private MockData: any;
+
   constructor(dom: HTMLDivElement, host: VisualNS.VisualHost, options: VisualNS.IVisualUpdateOptions) {
     super(dom, host, options);
-    Swiper.use([Autoplay]);
-    document.getElementById('visualDom').innerHTML = `
+    this.container = dom;
+    SwiperCore.use([Autoplay, Navigation, Pagination, EffectCoverflow, EffectFade, EffectCube, EffectFlip]);
+    this.container.innerHTML = `
     <div class="swiper-container mySwiper">
       <div class="swiper-wrapper">
-        <div class="swiper-slide">Slide 1</div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
-        <div class="swiper-slide">Slide 4</div>
-        <div class="swiper-slide">Slide 5</div>
-        <div class="swiper-slide">Slide 6</div>
-        <div class="swiper-slide">Slide 7</div>
-        <div class="swiper-slide">Slide 8</div>
-        <div class="swiper-slide">Slide 9</div>
       </div>
       <div class="swiper-button-next"></div>
       <div class="swiper-button-prev"></div>
       <div class="swiper-pagination"></div>
     </div>`;
-    var swiper = new Swiper(".swiper-container", {
-      spaceBetween: 30,
-      centeredSlides: true,
+    this.nextBtn = document.getElementsByClassName('swiper-button-next')[0];
+    this.prevBtn = document.getElementsByClassName('swiper-button-prev')[0];
+    this.swiperOptions = {
+      initialSlide: 1,
+      speed: 1000,
       autoplay: {
-        delay: 2500,
+        delay: 3000,
         disableOnInteraction: false,
       },
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      grabCursor: true,
+      centeredSlides: true,
+      spaceBetween: 30,
       loop: true,
       pagination: {
         el: ".swiper-pagination",
@@ -40,22 +80,134 @@ export default class Visual extends WynVisual {
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-      }
+      },
+    }
+    this.prevEffect = 'fadeEffect';
+    this.firstRender = true;
+    this.MockData = Visual.prepareMockData(host);
+  }
 
-    });
+  private static prepareMockData(host) {
+    return [
+      {
+        imageUrl: host.assetsManager.getImage("image1"),
+        imageDescription : '图片1'
+      },
+      {
+        imageUrl: host.assetsManager.getImage("image2"),
+        imageDescription : '图片2'
+      },
+      {
+        imageUrl: host.assetsManager.getImage("image3"),
+        imageDescription : '图片3'
+      },
+      {
+        imageUrl: host.assetsManager.getImage("image4"),
+        imageDescription : '图片4'
+      }
+    ]
   }
 
   public update(options: VisualNS.IVisualUpdateOptions) {
+    const dataViews = options.dataViews;
+    this.properties = options.properties;
+    this.isMock = !dataViews.length;
+    let bindData = [];
+    if (dataViews.length) {
+      const plainData = dataViews[0].plain.data || [];
+      const profileItems = dataViews[0].plain.profile;
+      const imageProfileName = profileItems.image.values[0].name;
+      const imageDescProfileName = profileItems.imageDescription.values[0] && profileItems.imageDescription.values[0].name;
+      bindData = plainData.map((data) => {
+        return {
+          imageUrl: data[imageProfileName],
+          imageDescription : data[imageDescProfileName] || ''
+        }
+      })
+    }
+    const swiperData = this.isMock ? this.MockData : bindData;
+    this.render(swiperData);
+  }
 
-    console.log(options);
+  private prepareStyle() {
+    let styleString = '';
+    for (const [key, value] of Object.entries(this.properties.imageDescSetting)) {
+      styleString += `${STYLENAME[key]}:${value};`
+    }
+    return styleString;
+  }
+
+  private prepareSlides(swiperData) {
+    let slides = [];
+    let styleString = this.prepareStyle();
+    slides = swiperData.map((data) => {
+      return `<div class="swiper-slide">
+                <img src="${data.imageUrl}" />      
+                <div class="swiper-desc" style="${styleString}">${data.imageDescription}</div>
+            </div>`
+    })
+    return slides;
+  }
+
+  private configSwiper() {
+    this.swiperOptions.initialSlide = this.firstRender ? 1 : 2;
+    this.firstRender = false;
+    this.swiperOptions.speed = this.properties.speed;
+    this.swiperOptions.autoplay.delay = this.properties.delay;
+    this.swiperOptions.effect = this.properties.effect;
+    delete this.swiperOptions[this.prevEffect];
+    const effectName = `${this.properties.effect}Effect`;
+    this.swiperOptions[effectName] = EFFECT[effectName];
+  }
+
+  private configButton() {
+    let buttonOpacity = this.properties.useButton ? 1 : 0;
+    this.nextBtn.style.opacity = buttonOpacity;
+    this.prevBtn.style.opacity = buttonOpacity;
+  }
+
+  private configAutoPlay() {
+    if (this.properties.autoPlay) {
+      this.swiper.autoplay.start();
+    } else {
+      this.swiper.autoplay.stop();
+    }
+  }
+
+  public render(swiperData): void {
+    this.container.style.opacity = this.isMock ? '0.5' : '1';
+    if (this.swiper) {
+      this.swiper.destroy(true, true);
+    }
+    this.configSwiper();
+    this.swiper = new Swiper(".swiper-container", this.swiperOptions);
+    this.swiper.removeAllSlides();
+    const slides = this.prepareSlides(swiperData);
+    this.swiper.appendSlide(slides);
+    this.configButton()
+    this.configAutoPlay()
+    this.swiper.update();
+  }
+
+  public onResize() {
+    if (this.swiper) {
+      this.swiper.updateSize();
+      this.swiper.update();
+    }
   }
 
   public onDestroy(): void {
-
+    if (this.swiper) {
+      this.swiper.destroy(true, true);
+    }
   }
 
   public getInspectorHiddenState(options: VisualNS.IVisualUpdateOptions): string[] {
-    return null;
+    let hiddenStates = [];
+    if (!options.properties.autoPlay) {
+      hiddenStates.push('delay');
+    }
+    return hiddenStates;
   }
 
   public getActionBarHiddenState(options: VisualNS.IVisualUpdateOptions): string[] {
