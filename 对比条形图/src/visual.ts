a61bbec4db605d27c67d95acb660b6cf6712af94
miya <2604574516@qq.com>
2021-08-20 16:24:32 +0800

    Fixed:Ranking color control

--- 对比条形图/src/visual.ts
+++ 对比条形图/src/visual.ts
@@ -197,6 +197,38 @@ export default class Visual {
     return;
   }
 
+  private setPosition(positionType: any) {
+    if (positionType === 'topLeft') {
+      return [0,-20]
+    }else if(positionType === 'topRight'){
+      return [350,-20]
+    }else if(positionType === 'bottomLeft'){
+      return [0,20]
+    }else if(positionType === 'bottomRight'){
+      return [350,20]
+    }else {
+      return positionType
+    }
+  }
+
+  private setRankingType(rankingType: any, targetIndex: any ) {
+    if(targetIndex <= 3){
+      let championList = ['冠','亚','季']
+      let excellentList = ['优','良','中']
+      if(rankingType === 'champion'){
+        let champion
+        return champion = championList.filter((ele,index) => (index+1) === targetIndex)
+      } else if(rankingType === 'excellent'){
+        let excellent
+        return excellent = excellentList.filter((ele,index) => (index+1) === targetIndex)
+      } else {
+        return targetIndex
+      } 
+    }else{
+      return targetIndex
+    }
+  }
+
   private render() {
     this.chart.clear();
     const options = this.properties;
@@ -218,6 +250,28 @@ export default class Visual {
       labelfontWeight = options.labelTextStyle.fontWeight
     }
 
+    const hexToRgba = (hex, opacity) => {
+      return 'rgba(' + parseInt('0x' + hex.slice(1, 3)) + ',' + parseInt('0x' + hex.slice(3, 5)) + ','
+              + parseInt('0x' + hex.slice(5, 7)) + ',' + opacity + ')';
+    }
+
+    const rgbaToHex = (color) => {
+      var values = color
+        .replace(/rgba?\(/, '')
+        .replace(/\)/, '')
+        .replace(/[\s+]/g, '')
+        .split(',');
+      var a = parseFloat(values[3] || 1),
+          r = Math.floor(a * parseInt(values[0]) + (1 - a) * 255),
+          g = Math.floor(a * parseInt(values[1]) + (1 - a) * 255),
+          b = Math.floor(a * parseInt(values[2]) + (1 - a) * 255);
+      return "#" +
+        ("0" + r.toString(16)).slice(-2) +
+        ("0" + g.toString(16)).slice(-2) +
+        ("0" + b.toString(16)).slice(-2);
+    }
+    
+
     let option = {
       tooltip: {
         show: true,
@@ -232,7 +286,7 @@ export default class Visual {
       },
       grid: {
         top: '0',
-        left: '15',
+        left: options.RankingPosition === 'left'? '65':'10',
         right: '4.75%',
         bottom: '0',
         containLabel: true
@@ -244,58 +298,23 @@ export default class Visual {
         axisTick: {
           show: false
         },
+        position: 'right',
         axisLabel: {
-          show: options.showLabel,
-          margin: 20,
-          width: 70,
-          color: options.textStyle.color,
-          fontSize: options.textStyle.fontSize.substr(0, 2),
-          fontWeight: _fontWeight,
-          fontFamily: options.textStyle.fontFamily,
-          fontStyle: options.textStyle.fontStyle,
-          formatter: (value, index) => {
-            if(options.showRanking && !this.isMock){
-              this.selectionSort(this.items, 'desc');
-              const _target  = this.rankingNumber&&this.rankingNumber.filter(element => value === element.name)[0];
-              if (_target.index <= 3) {  
-                return '{idx' + _target.index + '|' + ( _target.index) + '} {title|' + value + '}'
-              }  else {
-                return '{idx|' +  _target.index + '} {title|' + value + '}'
+          show: options.showBarLabel,
+          formatter: (params) => {
+            let dataRatio = ''
+            this.items[0].map((element,index) => {
+              if(params === element){
+                dataRatio = `${this.items[3][index].toFixed(1)}%`
               }
-            }else{
-              return value
-            }
+            })
+            return dataRatio
           },
-          rich: {
-            idx1: {
-                color: '#FB375E',
-                backgroundColor: '#FFE8EC',
-                borderRadius: 100,
-                padding: [2, 4]
-            },
-            idx2: {
-                color: '#FF9023',
-                backgroundColor: '#FFEACF',
-                borderRadius: 100,
-                padding: [2, 4]
-            },
-            idx3: {
-                color: '#01B599',
-                backgroundColor: '#E1F7F3',
-                borderRadius: 100,
-                padding: [2, 4]
-            },
-            idx: {
-                color: 'white',
-                borderRadius: 100,
-                // padding: [2, 2]
-            },
-            title: {
-                // backgroundColor:'blue',
-                // padding: [2, 2],
-
-            }
-          },
+          color: options.labelTextStyle.color,
+          fontSize: options.labelTextStyle.fontSize.substr(0, 2),
+          fontWeight: labelfontWeight,
+          fontFamily: options.labelTextStyle.fontFamily,
+          fontStyle: options.labelTextStyle.fontStyle,
         },
         axisLine: {
           show: false
@@ -318,16 +337,71 @@ export default class Visual {
         barWidth: options.barWidth,
         data: items[3],
         label: {
-          show: options.showBarLabel,
-          position: 'right',
-          formatter: (params) => {
-            return params.value.toFixed(1) + "%";
+          show: options.showLabel,
+          margin: 1,
+          position: this.setPosition(options.RankingPosition) ,
+          rotate : options.rotationDegree,
+          
+          formatter: (value) => {
+            if(options.showRanking && !this.isMock){
+              this.selectionSort(this.items, 'desc');
+              const _targetCopy  = this.rankingNumber&&this.rankingNumber.filter(element => value.name === element.name)[0];
+              const _target = JSON.parse(JSON.stringify(_targetCopy))
+              _target.index = this.setRankingType(options.rankingType, _target.index)
+              const targetCopyIndex = _targetCopy.index
+              if (targetCopyIndex <= 3) { 
+                return '{idx' + targetCopyIndex + '|' +  _target.index + '} {title|' + value.name + '}'
+              }  else {
+                return '{idx|' +  _target.index + '} {title|' + value.name + '}'
+              }
+            }else{
+              return `{title|${value.name}}`
+            }
           },
-          color: options.labelTextStyle.color,
-          fontSize: options.labelTextStyle.fontSize.substr(0, 2),
-          fontWeight: labelfontWeight,
-          fontFamily: options.labelTextStyle.fontFamily,
-          fontStyle: options.labelTextStyle.fontStyle,
+          rich: {
+            idx1: {
+                color: options.rankingColor[0],
+                backgroundColor: rgbaToHex(hexToRgba(options.rankingColor[0],0.2)),
+                borderRadius: options.rankingShape === 'circular' ? 100 : '',
+                padding: [4, 4],
+                width:10,
+                height:10,
+                align: 'center',
+            },
+            idx2: {
+                color: options.rankingColor[1],
+                backgroundColor: rgbaToHex(hexToRgba(options.rankingColor[1],0.2)),
+                borderRadius: options.rankingShape === 'circular' ? 100 : '',
+                padding: [4, 4],
+                width:10,
+                height:10,
+                align: 'center',
+            },
+            idx3: {
+                color: options.rankingColor[2],
+                backgroundColor: rgbaToHex(hexToRgba(options.rankingColor[2],0.2)),
+                borderRadius: options.rankingShape === 'circular' ? 100 : '',
+                padding: [4, 4],
+                width:10,
+                height:10,
+                align: 'center',
+            },
+            idx: {
+                color: 'white',
+                borderRadius: 100,
+                width:10,
+                height:10,
+                align: 'center',
+                padding: [2, 4]
+            },
+            title: {
+              color: options.textStyle.color,
+              fontSize: options.textStyle.fontSize.substr(0, 2),
+              fontWeight: _fontWeight,
+              fontFamily: options.textStyle.fontFamily,
+              fontStyle: options.textStyle.fontStyle,
+            }
+          },
         },
         itemStyle: {
           color: new echarts.graphic.LinearGradient(1, 0, 0, 0, [{
@@ -383,3 +457,17 @@ export default class Visual {
     console.log(name);
   }
 }
+
+
+// {
+//   "name": "RankingDeviationX",
+//   "type": "Integer",
+//   "displayName": "左右偏移",
+//   "defaultValue": -50
+// },
+// {
+//   "name": "RankingDeviationY",
+//   "type": "Integer",
+//   "displayName": "上下偏移",
+//   "defaultValue": 0
+// },
