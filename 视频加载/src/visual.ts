5bd2c5a593f62517e236dfecd305a702b1fffe68
xiaoyao-xiaolv <xiaoyaoxiaolv@gmail.com>
2021-11-22 16:56:11 +0800

    更新视频播放组件

--- 视频加载/src/visual.ts
+++ 视频加载/src/visual.ts
@@ -2,14 +2,24 @@ import '../style/visual.less';
 export default class Visual {
   private container: HTMLDivElement;
   private properties: any;
+  private items: any;
   constructor(dom: HTMLDivElement, host: any) {
     this.container = dom;
+    this.items = '';
     this.properties = {
       videoUrl: 'http://video1.grapecity.com.cn/WynEnterprise/online/wyn3.0.mp4',
     };
   }
 
   public update(options: any) {
+    const dataView = options.dataViews[0];
+    this.items = '';
+    if (dataView &&
+      dataView.plain.profile.dimensions.values.length) {
+      const plainData = dataView.plain;
+      let dimensions = plainData.profile.dimensions.values;
+      this.items=plainData.data[0][dimensions[0].display];
+    }
     this.properties = options.properties;
     this.render();
   }
@@ -17,6 +27,7 @@ export default class Visual {
   private render() {
     this.container.innerHTML = "";
     const options = this.properties;
+    const items = options.custom ? options.videoUrl : this.items;
     let video: any = document.createElement("video");
     video.muted = false;
     video.setAttribute("controls", "controls");
@@ -25,14 +36,20 @@ export default class Visual {
     video.setAttribute("preload", "auto");
     video.setAttribute("height", "100%");
     video.setAttribute("width", "100%");
-    video.setAttribute("src", options.videoUrl);
+    video.setAttribute("src", items);
     this.container.appendChild(video);
   }
 
-  public getInspectorVisibilityState(properties: any): string[] {
+  // 自定义属性可见性
+  public getInspectorHiddenState(updateOptions: any): string[] {
+    if (!updateOptions.properties.custom) {
+      return ['videoUrl'];
+    }
     return null;
   }
-  public getActionBarVisibilityState(updateOptions: any): string[] {
+
+  // 功能按钮可见性
+  public getActionBarHiddenState(updateOptions: any): string[] {
     return null;
   }
   public onActionEventHandler = (name: string) => {
