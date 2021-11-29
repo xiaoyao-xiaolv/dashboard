134a3b48baaf34f424371a962f60544b3ef22529
Felix <Felix.Li@grapecity.com>
2021-11-29 15:38:25 +0800

    右键跳转 左键功能 数据格式 tooltip设置

--- boxplot/src/visual.ts
+++ boxplot/src/visual.ts
@@ -22,8 +22,7 @@ export default class Visual extends WynVisual {
   private dimension: string
   private ActualValue: Array<any>
   private Series: string
-
-
+  private format: any;
 
   constructor(dom: HTMLDivElement, host: VisualNS.VisualHost, options: VisualNS.IVisualUpdateOptions) {
     super(dom, host, options)
@@ -43,7 +42,7 @@ export default class Visual extends WynVisual {
   private showTooltip = _.debounce((params, asModel = false) => {
     if (asModel) isTooltipModelShown = true;
     const fieldsName = [params.name + (params.seriesName), 'Min', 'Q1', 'Median', 'Q3', 'Max']
-    const fields = params.data.map((item, index) => { return { label: fieldsName[index], value: index ? item : '' } })
+    const fields = params.data.map((item, index) => { return { label: fieldsName[index], value: index ? this.formatData(item) : '' } })
     this.host.toolTipService.show({
       position: {
         x: params.event.event.x,
@@ -74,6 +73,8 @@ export default class Visual extends WynVisual {
         this.selection.forEach(i => this.dispatch('downplay', i));
         this.selection = [];
         this.selectionManager.clear();
+        this.host.toolTipService.hide();
+        this.host.contextMenuService.hide();
         return;
       }
     })
@@ -83,30 +84,84 @@ export default class Visual extends WynVisual {
       this.hideTooltip();
     })
 
+    //鼠标左键
     this.chart.on('click', (params) => {
-
-      if (params.componentType !== 'series') return;
-
-      this.showTooltip(params, true);
-
-      params.event.event.seriesClick = true;
-
-      const selectInfo = {
-        seriesIndex: params.seriesIndex,
-        dataIndex: params.dataIndex,
-      };
-      if (this.items[3][params.dataIndex]) {
-        const sid = this.items[3][params.dataIndex];
-        this.selectionManager.select(sid, true);
+      this.host.contextMenuService.hide();
+      params.event.event.stopPropagation();
+      if (params.event.event.button == 0) {
+        //鼠标左键功能
+        let leftMouseButton = this.properties.leftMouseButton;
+        switch (leftMouseButton) {
+          //鼠标联动设置    
+          case "none": {
+            if (this.properties.onlySelect) {
+              if (!this.selectionManager.contains(this.items[3][params.dataIndex])) {
+                this.selection = [];
+                this.selectionManager.clear();
+                this.selection.push(this.items[3][params.dataIndex]);
+              } else {
+                this.selection = [];
+                this.selectionManager.clear();
+              }
+            } else {
+              if (!this.selectionManager.contains(this.items[3][params.dataIndex])) {
+                this.selection.push(this.items[3][params.dataIndex]);
+              } else {
+                this.selection.splice(this.selection.indexOf(this.items[3][params.dataIndex]), 1);
+                this.selectionManager.clear(this.items[3][params.dataIndex])
+                return
+              }
+            }
+            this.selectionManager.select(this.selection, true);
+            if (this.selection.length == this.items[0].length) {
+              this.selectionManager.clear(this.selection);
+              this.selection = [];
+            }
+            break;
+          }
+          case "showToolTip": {
+            this.showTooltip(params, true);
+            break;
+          }
+          default: {
+            const selectionIds = this.items[3][params.dataIndex];
+            this.host.commandService.execute([{
+              name: leftMouseButton,
+              payload: {
+                selectionIds,
+                position: {
+                  x: params.event.event.x,
+                  y: params.event.event.y,
+                },
+              }
+            }])
+          }
+        }
       }
-      this.dispatch('highlight', selectInfo);
-      this.selection.push(selectInfo)
-
     })
+
+    this.chart.on('mouseup', (params) => {
+      if (params.event.event.button === 2) {
+        document.oncontextmenu = function () { return false; };
+        params.event.event.preventDefault();
+        this.host.contextMenuService.show({
+          position: {								
+            x: params.event.event.x,
+            y: params.event.event.y,
+          },
+          menu: true
+        }, 10)
+        return;
+      }else{
+        this.host.contextMenuService.hide();	
+      }
+    })
+
 
   }
 
   public update(options: VisualNS.IVisualUpdateOptions) {
+    this.format = options.dataViews[0].plain.profile.ActualValue.values[0].format;
     const dataView = options.dataViews[0];
     this.items = [];
     if (dataView &&
@@ -152,7 +207,6 @@ export default class Visual extends WynVisual {
       this.isMock = true;
     }
     this.properties = options.properties;
-
     this.render()
   }
 
@@ -232,7 +286,17 @@ export default class Visual extends WynVisual {
             color: options.boxFillColor,
             borderColor: options.boxBorderColor[index]
           },
-          tooltip: { formatter: formatter }
+          tooltip: { 
+            formatter: (param) => {
+            return [
+              `${'分类'}` + param.name + ': ',
+              'Max: ' + this.formatData(param.data[5]),
+              'Q3: ' + this.formatData(param.data[4]),
+              'Median: ' + this.formatData(param.data[3]),
+              'Q1: ' + this.formatData(param.data[2]),
+              'Min: ' + this.formatData(param.data[1])
+            ].join('<br/>')
+          } }
         }
       })
     }
@@ -344,16 +408,7 @@ export default class Visual extends WynVisual {
       ]
     };
 
-    const formatter = (param) => {
-      return [
-        `${'分类'}` + param.name + ': ',
-        'Max: ' + param.data[5],
-        'Q3: ' + param.data[4],
-        'Median: ' + param.data[3],
-        'Q1: ' + param.data[2],
-        'Min: ' + param.data[1]
-      ].join('<br/>')
-    }
+   
 
     this.chart.setOption(option)
   }
@@ -395,5 +450,11 @@ export default class Visual extends WynVisual {
 
   public getActionBarHiddenState(options: VisualNS.IVisualUpdateOptions): string[] {
     return null;
+  }
+
+  private formatData (number) {
+    const formatService = this.host.formatService;
+    let realDisplayUnit = formatService.getAutoDisplayUnit([number]);
+    return formatService.format(this.format, number, realDisplayUnit);
   }
 }
